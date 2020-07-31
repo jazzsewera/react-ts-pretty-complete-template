ac6a02f625448954ab506452931bb92fe24f30fd
Błażej Sewera <blazejok1@wp.pl>
2020-07-31 20:33:09 +0200

    First commit

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+// prettier.config.js or .prettierrc.js
+module.exports = {
+  printWidth: 120,
+  trailingComma: 'es5',
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  eslintIntegration: true,
+};
